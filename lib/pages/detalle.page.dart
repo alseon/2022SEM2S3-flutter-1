2423787a9a2390abb1a5ae76b3b2ccb7caed5f45
asleon <alexander.suarez@unillanos.edu.co>
2022-09-17 11:58:09 -0500

    detalle 2

---
+++ lib/pages/detalle.page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:newnoticias/models/article.model.dart';
+
+class DetallePage extends StatelessWidget {
+  DetallePage({super.key, required this.article});
+  ArticleModel article;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(article.title)),
+        body: Container(
+          child: Text(article.content),
+        ));
+  }
+}
--- lib/widgets/card.widget.dart
+++ lib/widgets/card.widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:newnoticias/models/article.model.dart';
+import 'package:newnoticias/pages/detalle.page.dart';
 
 class CardWidget extends StatelessWidget {
   CardWidget({super.key, required this.article});
@@ -11,15 +12,25 @@ class CardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-          color: Color.fromARGB(255, 29, 78, 87),
-          height: 100,
-          child: Column(
-            children: [
-              Text(article.author),
-              Text(article.content),
-            ],
-          )),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => DetallePage(
+                        article: article,
+                      )));
+        },
+        child: Container(
+            color: Color.fromARGB(255, 29, 78, 87),
+            height: 100,
+            child: Column(
+              children: [
+                Text(article.author),
+                Text(article.content),
+              ],
+            )),
+      ),
     );
   }
 }
